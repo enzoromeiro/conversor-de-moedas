19a5c1aaccbce9b9ac73e9faa825f6d1ab16e1e6
Enzo Romeiro <enzoromeiro17@gmail.com>
2023-06-02 14:39:02 -0300

    corrigindo erro de lógica

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+
 function botao() {
     let valor = Number(document.getElementById ('valor').value)
     let moedas = document.getElementById ('moedas').value
     let resultado = 0
     let total = document.getElementById ('total')
 
+    if (!valor === true) {
+        alert ('Insira um Valor')
+    } else if (moedas === 'Selecione') {
+        alert ('Escolha uma moeda')
+    }
+
     if (moedas === 'Dolar') {
-        resultado = valor * 5
+        resultado = valor / 5
         total.innerHTML = `$ ${resultado}`
     } else if (moedas === 'Euro') {
-        resultado = valor * 5.50
-        total.innerHTML = `€ ${resultado}`
+        resultado = valor / 5.50
+        total.innerHTML = `€ ${resultado.toFixed(2)}`
     }
 }
 
